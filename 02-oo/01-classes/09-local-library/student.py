c5101e2f4adc498a149d1e4dc09b8aab0a1e212d
Thomas Van Gompel <thomas.vangompel@student.ucll.be>
2024-03-11 16:58:53 +0100

    02-oo/01 afgemaakt

--- 02-oo/01-classes/09-local-library/student.py
+++ 02-oo/01-classes/09-local-library/student.py
@@ -1,17 +1,25 @@
 class Book:
     def __init__(self, title, author):
-        pass
+        self.title = title
+        self.author = author
 
 
 class Library:
     def __init__(self, name):
-        pass
+        self.name = name
+        self.books = []
 
     def add_book(self, book):
-        pass
+        self.books.append(book)
 
     def remove_book(self, book):
-        pass
+        for bk in self.books:
+            if bk.title == book.title and bk.author == book.author:
+                self.books.remove(bk)
 
     def search_books(self, search_string):
-        pass
+        result = []
+        for book in self.books:
+            if (search_string.lower() in book.title.lower() or search_string.lower() in book.author.lower()):
+                result.append(book)
+        return result
